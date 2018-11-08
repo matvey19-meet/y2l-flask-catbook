dd3477ba3534360d20a118e4936626624b3d8a03
george18-meet <abudauodg@gmail.com>
2018-11-08 19:30:42 +0200

    msg

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from database import get_all_cats
+from database import *
 
 app = Flask(__name__)
 
@@ -9,5 +9,10 @@ def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
 
+@app.route('/cats/<int:cat_id>')
+def cb_dts(cat_id):
+    cat = get_cat(cat_id)
+    return render_template("cat.html", cat=cat)
+    
 if __name__ == '__main__':
    app.run(debug = True)
